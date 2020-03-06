0a76c7e563c8a2010b7a64d5b9caa707f5e6f615
Александр Осипов <aleosi@yandex-team.ru>
2020-03-06 23:40:42 +0300

    matrix mul openCL

--- main.c
+++ main.c
@@ -298,21 +298,19 @@ int main() {
 #pragma omp parallel
     {
 
+        // вычисления на транспонированной матрице
 #pragma omp for schedule(static, 1)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < p; j++) {
                 float tt = 0;
                 for (int k = 0; k < m; k++) {
-//                    if(i * m + k >= n * m || i * m + k < 0 || j * m + k >= m * p || j * m + k < 0){
-//                        printf("Error %d %d %d\n", i, j, k);
-//                        exit(1);
-//                    }
                     tt += matrix1[i * m + k] * matrix21[j * m + k];
                 }
                 matrix31[i * p + j] = tt;
             }
         }
 
+        // вычисления на обычной матрице
 //#pragma omp for schedule(static, 1)
 //        for (int i = 0; i < n; i++) {
 //            for (int j = 0; j < p; j++) {
@@ -362,86 +360,4 @@ int main() {
 
 
     return 0;
-
-
-//    const int n = 10;
-//    const int m = 20;
-//    const int p = 30;
-//
-//    float *matrix1 = (float *) malloc(n * m * sizeof(float));
-//    float *matrix2 = (float *) malloc(m * p * sizeof(float));
-//    float *matrix21 = (float *) malloc(m * p * sizeof(float));
-//    float *matrix3 = (float *) malloc(n * p * sizeof(float));
-//
-//    for (int i = 0; i < n * m; i++) {
-//        matrix1[i] = i;
-//    }
-//    for (int i = 0; i < m * p; i++) {
-//        matrix2[i] = i + n * m;
-//    }
-//
-//    struct timeval stop, start;
-//    gettimeofday(&start, NULL);
-//
-//
-//
-//
-//
-//
-//
-////    printMatrix(matrix2, m, p);
-////
-////    for(int i = 0; i < m * p; i++){
-////        int n1 = i / p;
-////        int n2 = i % p;
-////
-////        int prev = n2 * m + n1;
-////
-//////        for(int j = i + 1; j < p; j++){
-////            if(n1 > n2){
-////                float temp = matrix2[i];
-////                matrix2[i] = matrix2[prev];
-////                matrix2[prev] = temp;
-////            }
-//////        }
-////    }
-//
-//
-//    for (int i = 0; i < m; i++) {
-//        for (int j = 0; j < p; j++) {
-//            matrix21[j * m + i] = matrix2[i * p + j];
-//        }
-//    }
-//
-////    printMatrix(matrix2, p, m);
-//
-//#pragma omp parallel
-//    {
-//
-//#pragma omp for schedule(static, 1)
-//        for (int i2 = 0; i2 < n * p; i2++) {
-//            int j = i2 % p;
-//
-//            float tt = 0;
-////            float* cur_matrix0 = matrix1 + i2 - j;
-////            float* cur_matrix1 = matrix21 + m * j;
-//            for (int k = 0; k < m; k++) {
-////                tt += cur_matrix0[k] * cur_matrix1[k];
-//                int in2 = k + m * j;
-//                tt += matrix1[i2 - j + k] * matrix21[in2];
-////                int in2 = k * p + j;
-////                tt += matrix1[i2 - j + k] * matrix2[in2];
-//            }
-//            matrix3[i2] = tt;
-//        }
-//    }
-//
-//    gettimeofday(&stop, NULL);
-//    long long int tt = ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000;
-//
-//    printf("someshit: %5.0f\n", matrix3[0]);
-//
-//    printf("time: %lld ms\n", tt);
-//
-//    return 0;
 }
